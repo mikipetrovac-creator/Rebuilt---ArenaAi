82a6d462624d93891f8edc3eff1a607987e21485
mikipetrovac-creator <mikipetrovac@gmail.com>
2026-09-24 09:32:50 +0300

    M.Y.V. 2.0: add scripts/tours.js

---
+++ scripts/tours.js
@@ -0,0 +1,6 @@
+window.MYV_TOURS=Object.freeze({
+  cappadocia:{id:"cappadocia",path:"/",duration:"2 days",currency:"EUR",packages:{budget:{adult:50,child:25,infant:0},premium:{adult:100,child:40,infant:0}}},
+  demre:{id:"demre",path:"/demre-myra-kekova",duration:"1 day",currency:"EUR",packages:{budget:{adult:55,child:28,infant:0},premium:{adult:80,child:40,infant:0}}},
+  pamukkale:{id:"pamukkale",path:"/pamukkale",duration:"1 day",currency:"EUR",packages:{budget:{adult:40,child:25,infant:0},premium:{adult:70,child:35,infant:0}}},
+  greenCanyon:{id:"green-canyon",path:"/green-canyon",duration:"1 day",currency:"EUR",packages:{standard:{adult:30,child:20,infant:0}}}
+});
